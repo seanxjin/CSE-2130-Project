9511e1ce94e906f80e9cdaa80f407314780ac678
Sean Jin <S.jin8@share.epsb.ca>
2022-11-25 11:32:53 -0700

    started

---
+++ main.py
@@ -0,0 +1,21 @@
+'''
+title: Large Mammal Populations Elk Island National Park
+author: Sean Jin
+date-created: 2022-11-25
+'''
+
+import pathlib
+import sqlite3
+
+# --- SUBROUNTINE --- #
+
+# -- INPUT -- #
+
+# -- PROCESSING -- #
+
+# -- OUTPUTS -- #
+
+# --- VARIABLE --- #
+
+
+if __name__ == "__main__":
