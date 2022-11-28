a532929a916502a1c66470c8cb7069bf08769bde
Sean Jin <S.jin8@share.epsb.ca>
2022-11-27 22:17:00 -0700

    Had a little breakthrough on test.py on join the commas in comments

---
+++ Test.py
@@ -0,0 +1,14 @@
+def setup(FILENAME):
+    FILE = open(FILENAME)
+    TEXTLIST = FILE.readlines()
+    FILE.close()
+    for i in range(len(TEXTLIST)):
+        if TEXTLIST[i][-1] == "\n":
+            TEXTLIST[i] = TEXTLIST[i][:-1]
+        TEXTLIST[i] = TEXTLIST[i].split(',')
+        if len(TEXTLIST[i]) > 19:
+            TEXTLIST[i][17] = ",".join([TEXTLIST[i][j] for j in [17, 18]])
+            TEXTLIST[i].pop(18)
+    return TEXTLIST
+
+print(setup("Elk_Island_NP_Grassland_Forest_Ungulate_Population_1906-2017_data_reg.txt"))
--- main.py
+++ main.py
@@ -10,6 +10,9 @@ import sqlite3
 # --- SUBROUNTINE --- #
 
 # -- INPUT -- #
+
+
+# -- PROCESSING -- #
 def setup(FILENAME):
     """
     Opens the file and extracts the contents of the file
@@ -23,17 +26,34 @@ def setup(FILENAME):
         if TEXTLIST[i][-1] == "\n":
             TEXTLIST[i] = TEXTLIST[i][:-1]
         TEXTLIST[i]= TEXTLIST[i].split(',')
+        if len(TEXTLIST[i]) > 19:
+            TEXTLIST[i] = " ".join(TEXTLIST[i])
         for j in range(len(TEXTLIST[i])):
             if TEXTLIST[i][j].isnumeric():
                 TEXTLIST[i][j] = int(TEXTLIST[i][j])
     return TEXTLIST
-
-# -- PROCESSING -- #
+def joinComments(TEXTLIST):
+    """
+    Joins the places where the comments are supposed to be joined, but instead they are separated by commas
+    :param TEXTLIST: list --> 2d array
+    :return: 2d array
+    """
+    for i in range(len(TEXTLIST)):
+        while len(TEXTLIST[i]) > 19:
+            TEXTLIST[i] = " ".join(TEXTLIST[i])
 
 # -- OUTPUTS -- #
 
 # --- VARIABLE --- #
-FILENAME = ""
+DATAFILE = "Population.db"
+
+FIRST_RUN = True
+
+if (pathlib.Path.cwd() / DATAFILE).exists():
+    FIRST_RUN = False
+CONNECTION = sqlite3.connect(DATAFILE)
+CURSOR = CONNECTION.cursor()
 
 if __name__ == "__main__":
-    print(setup("Elk_Island_NP_Grassland_Forest_Ungulate_Population_1906-2017_data_reg.txt"))
+    TEXTLIST = print(setup("Elk_Island_NP_Grassland_Forest_Ungulate_Population_1906-2017_data_reg.txt"))
+
